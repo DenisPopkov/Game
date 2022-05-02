6769054706d763ad9f98d0ff26ce2e6174a564b7
Denis Popkov <d.popkov@ltst.ru>
2022-05-02 13:40:25 +0600

    refactor

--- cwin.c
+++ cwin.c
@@ -166,23 +166,7 @@ void outputcolor(int foreground, int background) {
   SetConsoleTextAttribute ( h, foreground | background );
 }
 
-// Изменяет весь цвет в программе
-void screencol(int x) {
-  HANDLE hStdOut;
-  HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  DWORD count;
-  DWORD cellCount;
-  COORD homeCoords = {0, 0};
-  hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-  if (hStdOut == INVALID_HANDLE_VALUE) return;
-  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
-  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
-  SetConsoleTextAttribute ( h, x | x );
-  if (!FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount, homeCoords, &count)) return;
-  if (!FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count)) return;
-  SetConsoleCursorPosition( hStdOut, homeCoords );
-}
+
 
 // Получает размер терминала
 int getTerminalDimensions(int *rows, int *columns) {
